f2347c735ae6c70b75f14addfada81273cce507e
stajun <stajun@naver.com>
2018-03-29 15:57:55 +0900

    Circle 클래스 테스트

--- JavaStudy/src/study/example1/Main.java
+++ JavaStudy/src/study/example1/Main.java
@@ -1,10 +1,17 @@
 package study.example1;
 
+import study.shape.Circle;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Circle c1 = new Circle();
+		c1.printCircle();
+		c1.moveCenter(10, 10);
+		c1.printCircle();
+		c1.resize(10.1);
+		c1.printCircle();
 	}
 
 }
