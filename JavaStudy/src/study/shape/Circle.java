2a32b4003163d6275bf5d221cb7ace0a3f88fc82
stajun <stajun@naver.com>
2018-03-29 15:18:29 +0900

    study.shape 패키지에 Circle 클래스 추가 및 getter, setter 추가

---
+++ JavaStudy/src/study/shape/Circle.java
@@ -0,0 +1,28 @@
+package study.shape;
+
+public class Circle {
+	private int centerX;
+	private int centerY;
+	private double radius;
+	public int getCenterX() {
+		return centerX;
+	}
+	public void setCenterX(int centerX) {
+		this.centerX = centerX;
+	}
+	public int getCenterY() {
+		return centerY;
+	}
+	public void setCenterY(int centerY) {
+		this.centerY = centerY;
+	}
+	public double getRadius() {
+		return radius;
+	}
+	public void setRadius(double radius) {
+		this.radius = radius;
+	}
+	
+	
+	
+}
