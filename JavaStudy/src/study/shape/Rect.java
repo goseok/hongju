390f3cde1aab30b7be0bcb87b3c22d708be8cb32
stajun <stajun@naver.com>
2018-03-29 17:24:28 +0900

    Rect 클래스 생성 및 생성자와 출력 메소드 구현(study.shape 패키지)

--- JavaStudy/src/study/shape/Rect.java
+++ JavaStudy/src/study/shape/Rect.java
@@ -3,7 +3,72 @@ package study.shape;
 public class Rect {
 	private Point leftUp;
 	private Point rightDown;
-	private int widht;
+	private int width;
 	private int height;
+	public Point getLeftUp() {
+		return leftUp;
+	}
+	public void setLeftUp(Point leftUp) {
+		this.leftUp = leftUp;
+	}
+	public Point getRightDown() {
+		return rightDown;
+	}
+	public void setRightDown(Point rightDown) {
+		this.rightDown = rightDown;
+	}
+	public int getWidth() {
+		return width;
+	}
+	public void setWidth(int widht) {
+		this.width = widht;
+	}
+	public int getHeight() {
+		return height;
+	}
+	public void setHeight(int height) {
+		this.height = height;
+	}
 	
+	private void calWidth() {
+		width = Math.abs(rightDown.getX() 
+				- leftUp.getX());
+	}
+	private void calHeight() {
+		height = Math.abs(rightDown.getY()
+				-leftUp.getY());
+	}
+	public Rect(){
+		this.leftUp = new Point(0,10);
+		this.rightDown = new Point(10,0);
+		this.calWidth();
+		this.calHeight();
+	}
+	public Rect(int left, int up, int right, int down) {
+		this.leftUp = new Point(left,up);
+		this.rightDown = new Point(right,down);
+		this.calHeight();
+		this.calWidth();
+	}
+	public Rect(Point leftUp, Point rightDown) {
+		this.leftUp = new Point(leftUp);
+		this.rightDown = new Point(rightDown);
+		this.calHeight();
+		this.calWidth();
+	}
+	public void print() {
+		System.out.println("--사각형의 정보--");
+		System.out.print("좌상의 점 : ");
+		leftUp.print();
+		System.out.print("우하의 점 : ");
+		rightDown.print();
+		System.out.println("가로 : " + width);
+		System.out.println("세로 : " + height);
+	}
 }
+
+
+
+
+
+
